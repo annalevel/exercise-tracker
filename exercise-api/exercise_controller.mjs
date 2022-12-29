f5d4213f0d4bae9c5498e2d299d95dcfd67946eb
Anna Level <anna.e.m.level@gmail.com>
2022-12-29 12:33:00 -0800

    First commit

---
+++ exercise-api/exercise_controller.mjs
@@ -0,0 +1,155 @@
+import 'dotenv/config';
+import express from 'express';
+import jwt from 'express-jwt'
+import jwks from 'jwks-rsa';
+import validator from 'validator';
+import moment from 'moment';
+import * as exercises from './exercise_model.mjs';
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+// Using Auth0 for authentication
+var jwtCheck = jwt.expressjwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 30,
+        jwksUri: process.env.JWKS_URI
+  }),
+  audience: process.env.AUDIENCE,
+  issuer: process.env.ISSUER,
+  algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
+/**
+ * Sanitizes and converts data to the appropriate formats.
+ * Note: missing or incorrect units will never yield an error. Anything other than 'lbs' will always be changed to 'kgs'.
+ * @returns object with the cleaned up data in the named fields.
+ */
+const cleanUpExercise = (name, reps, weight, unit, date) => {
+    name = name === undefined ? '' : validator.escape(name).trim();
+    
+    reps = parseInt(reps);
+    weight = parseInt(weight);
+
+    if (unit !== 'lbs') {
+        unit = 'kgs';
+    }
+    
+    date = moment(date).format('YYYY-MM-DD');
+
+    return { name, reps, weight, unit, date };
+};
+
+const validateDate = (date) => {
+    if (date === undefined) {
+        return false;
+    }
+    const mDate = moment(date);
+    return mDate.isValid();
+}
+
+/**
+ * Create a new exercise with the name, reps, weight, unit, and date provided in the body.
+ */
+app.post('/exercises', async (req, res, next) => {
+    if (validateDate(req.body.date) === true) {
+        const cleanedExercise = cleanUpExercise(req.body.name, req.body.reps, req.body.weight, req.body.unit, req.body.date);
+        cleanedExercise.user = req.auth.sub;
+
+        exercises.createExercise(cleanedExercise)
+            .then(exercise => {
+                return res.status(201).json(exercise);
+            })
+            .catch(error => {
+                return res.status(400).json({ Error: 'Invalid request' });
+            });
+    } else {
+        return res.status(400).json({ Error: 'Invalid request' });
+    }
+});
+
+
+/**
+ * Retrieve the exercise corresponding to the ID provided in the URL.
+ */
+app.get('/exercises/:_id', (req, res) => {
+    exercises.getOneExercise(req.params._id)
+        .then(exercise => {
+            if (exercise === null) {
+                res.status(404).json({ Error: 'Not found' });
+            } else if (exercise.user !== req.auth.sub) {
+                res.status(401).json({ Error: 'Unauthorized' });
+            } else {
+                res.status(200).json(exercise);
+            }
+        })
+        .catch(error => {
+            res.status(400).json({ Error: 'Invalid request' });
+        });
+});
+
+/**
+ * Retrieve all exercises.
+ */
+app.get('/exercises', (req, res) => {
+    exercises.getAllExercises(req.auth.sub)
+        .then(exercise => {
+            res.status(200).json(exercise);
+        })
+        .catch(error => {
+            res.status(400).json({ Error: 'Request failed' });
+        });
+});
+
+/**
+ * Update the exercise whose id is provided in the path parameter and set
+ * its properties to the values provided in the body.
+ */
+app.put('/exercises/:_id', (req, res, next) => {
+    if (validateDate(req.body.date) === true) {
+        const cleanedExercise = cleanUpExercise(req.body.name, req.body.reps, req.body.weight, req.body.unit, req.body.date);
+
+        exercises.updateExercise(req.params._id, req.auth.sub, cleanedExercise)
+        .then(exercise => {
+            if (exercise === null) {
+                res.status(404).json({ Error: 'Not found' });
+            } else if (exercise.user !== req.auth.sub) {
+                res.status(401).json({ Error: 'Unauthorized' });
+            } else {
+                res.status(200).json(exercise);
+            }
+        })
+        .catch(error => {
+            res.status(400).json({ Error: 'Invalid request' });
+        });
+    } else {
+        res.status(400).json({ Error: 'Invalid request' });
+    }
+});
+
+/**
+ * Delete the exercise whose id is provided in the query parameters
+ */
+app.delete('/exercises/:_id', (req, res) => {
+    exercises.deleteExercise(req.params._id, req.auth.sub)
+        .then(exercise => {
+            if (exercise.deletedCount === 1) {
+                res.status(204).send();
+            } else {
+                res.status(404).json({ Error: 'Not found' });
+            }
+        })
+        .catch(error => {
+            res.status(404).json({ Error: 'Not found' });
+        });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}.`);
+});
---
+++ exercise-api/exercise_model.mjs
@@ -0,0 +1,96 @@
+import 'dotenv/config';
+import mongoose from 'mongoose';
+
+mongoose.connect(
+    process.env.MONGODB_CONNECT_STRING,
+    { useNewUrlParser: true }
+);
+
+// Connect to to the database
+const db = mongoose.connection;
+// The open event is called when the database connection successfully opens
+db.once("open", () => {
+    console.log("Successfully connected to MongoDB using Mongoose!");
+});
+
+/**
+ * Defines the schema
+ */
+const exerciseSchema = mongoose.Schema({
+    user: { type: String, required: true },
+    name: { type: String, required: true, minLength: 1 },
+    reps: { type: Number, required: true, min: 1 },
+    weight: { type: Number, required: true, min: 1 },
+    unit: { type: String, required: true, enum: ['kgs', 'lbs'] },
+    date: { type: String, required: true }
+});
+
+/**
+ * Compile the model from the schema. This must be done after defining the schema.
+ */
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+/**
+ * Inserts an exercise into the database.
+ * 
+ * @exercise {object} object with the following properties:
+ *                    user: id of user, not provided by user
+ *                    name: Name of exercise
+ *                    reps: Number of times it was performed
+ *                    weight: Weight used
+ *                    unit: "lbs" or "kgs"
+ *                    date: Date performed
+ * @returns Promise of the newly inserted exercise document from MongoDB
+ */
+const createExercise = (exercise) => {
+    const exerciseToInsert = new Exercise(exercise);
+    return exerciseToInsert.save();
+};
+
+/**
+ * Returns all exercises in the database.
+ * @user {String} User id to find the exercises for
+ * @returns Promise of a list of all exercise documents in the database
+ */
+const getAllExercises = (user) => {
+    const query = Exercise.find({ user: user });
+    return query.exec();
+};
+
+/**
+ * Returns a single exercise in the database by ID.
+ * @id {number} ID number of exercise to return
+ * @returns Promise of the document for the corresponding exercise
+ */
+const getOneExercise = (id) => {
+    const query = Exercise.findOne({_id: id});
+    return query.exec();
+};
+
+/**
+ * Update a single exercise with the given fields and values.
+ * @id {number} ID number of exercise
+ * @user {String} User id the exercise must be
+ * @fieldsToUpdate {object} Object consisting of fields and what to update their values to
+ * @returns 
+ */
+const updateExercise = async (id, user, fieldsToUpdate) => {
+    const query = Exercise.updateOne({_id: id, user: user}, fieldsToUpdate);
+    await query.exec();
+
+    const updatedExercise = Exercise.findOne({_id: id});
+    return updatedExercise.exec();
+};
+
+/**
+ * Deletes a single exercise in the database with the given ID.
+ * @id {number} id ID number of exercise
+ * @user {String} User id the exercise must be
+ * @returns Promise of JSON response for data on documents deleted
+ */
+const deleteExercise = (user, id) => {
+    const query = Exercise.deleteOne({_id: id, user: user});
+    return query;
+};
+
+export {createExercise, getAllExercises, getOneExercise, updateExercise, deleteExercise}
